52178884358dd3128bbcc261c2d92b4394f7dd4a
ZaheerAli12134 <zaheermirali121@gmail.com>
2026-03-10 11:51:34 +0000

    Update Weather.js, allowed for longitude and latitude parameters to be passed

--- src/Weather.js
+++ src/Weather.js
@@ -1,19 +1,33 @@
 import { fetchWeatherApi } from 'openmeteo';
+import { useEffect } from 'react';
 
-const Weather = () => {
-    const params = {
-    latitude: [52.54],
-    longitude: [13.41],
-    current: 'temperature_2m,weather_code,wind_speed_10m,wind_direction_10m',
-    hourly: 'temperature_2m,precipitation',
-    daily: 'weather_code,temperature_2m_max,temperature_2m_min'
-    };
-    const url = 'https://api.open-meteo.com/v1/forecast';
-    (async () => {
-    const responses = await fetchWeatherApi(url, params);
-    const current = responses[0].current();
-    console.log("Current Temperature:", current.variables(0).value(), "°C");
-    })();
-};
+const Weather = ({latitude, longitude}) => {
+
+    useEffect(() =>{
+
+        if(latitude === '' || longitude === ''){
+            return
+        }
+
+        const params = {
+            latitude: [latitude],
+            longitude: [longitude],
+            current: 'temperature_2m,weather_code,wind_speed_10m,wind_direction_10m',
+            hourly: 'temperature_2m,precipitation',
+            daily: 'weather_code,temperature_2m_max,temperature_2m_min'
+            };
+        const url = 'https://api.open-meteo.com/v1/forecast';
+        (async () => {
+        const responses = await fetchWeatherApi(url, params);
+        if(!responses[0]){
+            return
+        }
+        const current = responses[0].current();
+        console.log("Current Temperataure:", current.variables(0).value(), "°C");
+        })();
+
+    }, [latitude,longitude]);
+    
+}
 
 export default Weather;
