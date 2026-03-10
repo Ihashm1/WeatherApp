166f82c44d13dc41de2f4440333ab6defb28204e
TyroneMak <tyronemakoni86@gmail.com>
2026-03-10 13:58:43 +0000

    Added the links for the apis and a forecastButton class to be used for buttons later

--- src/Weather.js
+++ src/Weather.js
@@ -47,3 +47,10 @@ const Weather = ({latitude, longitude}) => {
 }
 
 export default Weather;
+//Master link for the forecast
+
+//https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,apparent_temperature_max,daylight_duration,sunrise,wind_speed_10m_max,wind_direction_10m_dominant&hourly=temperature_2m,visibility,wind_speed_10m,apparent_temperature,precipitation_probability,wind_direction_10m,precipitation,wind_gusts_10m,temperature_80m&current=temperature_2m,precipitation,wind_speed_10m,wind_direction_10m,wind_gusts_10m,apparent_temperature&wind_speed_unit=mph
+
+//Master link for the sea forecast
+
+//https://marine-api.open-meteo.com/v1/marine?latitude=54.544587&longitude=10.227487&daily=wave_height_max,wave_direction_dominant,swell_wave_height_max,swell_wave_direction_dominant&hourly=wave_height,sea_level_height_msl,wave_direction,swell_wave_height,swell_wave_direction,sea_surface_temperature&current=wave_height,wave_direction,sea_level_height_msl,sea_surface_temperature,swell_wave_direction,swell_wave_height
---
+++ src/forecastButton.js
@@ -0,0 +1,9 @@
+class forecastButton extends React.Component {
+  constructor(color,) {
+    super();
+    this.state = {color: "red"};
+  }
+  render() {
+    return <button>I am a button</button>;
+  }
+}
