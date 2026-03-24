53177be959c46d208699d67b6eb0442217d5b7d6
TyroneMak <tyronemakoni86@gmail.com>
2026-03-24 15:10:23 +0000

    Implemented switchVisibility which switches the visibility of 2 elements

--- src/App.js
+++ src/App.js
@@ -7,51 +7,70 @@ import ForecastButton from './forecastButton';
 import LineChart from './graphDisplay';
 
 const App = () => {
-
+    const [divDisp, setDivDisp] = useState(false);
     const [geoData, setGeoData] = useState('');
     const [weatherData, setWeatherData] = useState('');
 
-    const drawGraph = () =>{
-        console.log("drawing");
+    const switchVisibility = () =>{
+        setDivDisp(!divDisp);
+        console.log("clicked");
     }
 
     return (
         <div>
-            <h1>Weather Forecast App</h1>
-            <Geocoding sendData={setGeoData}/>
-            {geoData ? (
-                <>
-                <h2>Name: {geoData.name}</h2>
-                    <p>Latitude: {geoData.latitude}</p>
-                    <p>Longitude: {geoData.longitude}</p>
-                <Weather
-                    sendData={setWeatherData}
-                    latitude={geoData.latitude}
-                    longitude={geoData.longitude}
-                />
-                <FutureWeather
+            <div id="Forecast" className={divDisp ? "" : "hidden"}>
+                <h1>Weather Forecast App</h1>
+                <Geocoding sendData={setGeoData}/>
+                {geoData ? (
+                    <>
+                    <h2>Name: {geoData.name}</h2>
+                        <p>Latitude: {geoData.latitude}</p>
+                        <p>Longitude: {geoData.longitude}</p>
+                    <Weather
+                        sendData={setWeatherData}
                         latitude={geoData.latitude}
                         longitude={geoData.longitude}
                     />
-                </>
-            ):(
-                <p>Loading weather</p>
-            )}
+                    <FutureWeather
+                            latitude={geoData.latitude}
+                            longitude={geoData.longitude}
+                        />
+                    </>
+                ):(
+                    <p>Loading weather</p>
+                )}
 
-            {weatherData?(
-                <>
-                <ForecastButton
-                    safetynum={0}
-                    numval={weatherData.forecast.current[2][1]}
-                    units={"C"}
-                    text={"Temperature"}
-                    click={drawGraph}
-                />
-                <p>{/*weatherData.forecast.current[2][1]*/}</p>
-                </>
-            ):(
-                <p>Weatherdata array:</p>
-            )}
+                {weatherData?(
+                    <>
+                    <ForecastButton
+                        safetynum={0}
+                        numval={weatherData.forecast.current[2][1]}
+                        units={"C"}
+                        text={"Temperature"}
+                        click={switchVisibility}
+                    />
+                    <p>{/*weatherData.forecast.current[2][1]*/}</p>
+                    </>
+                ):(
+                    <p>Weatherdata array:</p>
+                )}
+            </div>
+            <div id="LineChart" className={divDisp ? "hidden" : ""}>
+                {weatherData?(
+                    <>
+                    <ForecastButton
+                        safetynum={0}
+                        numval={weatherData.forecast.current[2][1]}
+                        units={"C"}
+                        text={"Temperature"}
+                        click={switchVisibility}
+                    />
+                    <p>{/*weatherData.forecast.current[2][1]*/}</p>
+                    </>
+                ):(
+                    <p>Weatherdata array:</p>
+                )}
+            </div>
         </div>
     );
 };
