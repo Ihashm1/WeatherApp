13876caec521b9a4f34b7cb0b49bcb9522a6e020
TyroneMak <tyronemakoni86@gmail.com>
2026-03-24 11:03:33 +0000

    Added default ForecastButton class to be used in the forecast

--- src/forecastButton.js
+++ src/forecastButton.js
@@ -1,9 +1,20 @@
-class forecastButton extends React.Component {
-  constructor(color,) {
-    super();
-    this.state = {color: "red"};
-  }
-  render() {
-    return <button>I am a button</button>;
-  }
-}
+import React from 'react';
+const safety = ["blue", "green", "orange", "red"];
+const ForecastButton = ({safetynum, numval, units, text}) => {
+  console.log("button");
+  return( 
+    <button style={{backgroundColor: safety[safetynum]}}>
+      <div>
+        <div>
+          {numval}{units}
+        </div>
+        <div>
+          {text}
+        </div>
+      </div>
+    </button>
+  );
+};
+
+export default ForecastButton;
+//state(0,3),float,units,text
