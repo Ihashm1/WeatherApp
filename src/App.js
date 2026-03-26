2f53d9432ef87f737e60992e7cb647911a2f6fa6
andijen <89939466+andijen@users.noreply.github.com>
2026-03-26 13:38:27 +0000

    added all forecast conditions into main home page

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ const App = () => {
     }
 
     return (
-        <div style={{backgroundColor:"#cbd2e3",height:"100vh"}} id="Forecast" className={divDisp ? "container-fluid p-3" : "container-fluid p-3 hidden"}>
+        <>
+        <div style={{backgroundColor:"#cbd2e3",minHeight:"100vh"}} id="Forecast" className={divDisp ? "container-fluid p-3" : "container-fluid p-3 hidden"}>
             <Geocoding sendData={setGeoData}/>
             
             {geoData && (
@@ -35,19 +36,88 @@ const App = () => {
                 </>
             )}
 
-            {weatherData && (
-                <>
-                <div className="row row-cols-2 row-cols-md-4 row-gap-2 column-gap-2 mx-auto justify-content-center">
+            <>
+                <div className="row row-cols-2 row-cols-md-4 row-gap-2 column-gap-2 mx-auto justify-content-center p-1">
                     <ForecastButton
                         safetynum={0}
-                        numval={weatherData.forecast.current[2][1]}
-                        units={"C"}
-                        text={"Temperature"}
+                        numval={weatherData ? weatherData.forecast.current[2][1] : "N/A"}
+                        units={"°C"}
+                        text={"Feels like:" + (weatherData ?  weatherData.forecast.current[7][1]: "N/A") + "°C" }
+                        click={switchVisibility}
+                    />
+                    <ForecastButton
+                        safetynum={0}
+                        numval={weatherData ? weatherData.forecast.current[3][1] : "N/A"}
+                        units={"mm"}
+                        text={"Precipitation"}
+                        click={switchVisibility}
+                    />
+                    <ForecastButton
+                        safetynum={0}
+                        numval={weatherData ? weatherData.forecast.current[4][1] : "N/A"}
+                        units={"mph"}
+                        text={"Wind Speed"}
+                        click={switchVisibility}
+                    />
+                    <ForecastButton
+                        safetynum={0}
+                        // this needs to be an arrow graphic
+                        numval={weatherData ? weatherData.forecast.current[5][1] : "N/A"}
+                        units={"°"}
+                        text={"Wind Direction"}
+                        click={switchVisibility}
+                    />
+                    <ForecastButton
+                        safetynum={0}
+                        numval={weatherData ? weatherData.forecast.current[6][1] : "N/A"}
+                        units={"mph"}
+                        text={"Wind Gusts"}
+                        click={switchVisibility}
+                    />
+                    <ForecastButton
+                        safetynum={0}
+                        numval={weatherData ? weatherData.marine.current[2][1] : "N/A"}
+                        units={"m"}
+                        text={"Wave Height"}
+                        click={switchVisibility}
+                    />
+                    <ForecastButton
+                        safetynum={0}
+                        numval={weatherData ? weatherData.marine.current[3][1] : "N/A"}
+                        units={"°"}
+                        text={"Wave Direction"}
+                        click={switchVisibility}
+                    />
+                    <ForecastButton
+                        safetynum={0}
+                        numval={weatherData ? weatherData.marine.current[4][1] : "N/A"}
+                        units={"m"}
+                        text={"Sea Level Height"}
+                        click={switchVisibility}
+                    />
+                    <ForecastButton
+                        safetynum={0}
+                        numval={weatherData ? weatherData.marine.current[5][1] : "N/A"}
+                        units={"°C"}
+                        text={"Sea Surface Temperature"}
+                        click={switchVisibility}
+                    />
+                    <ForecastButton
+                        safetynum={0}
+                        numval={weatherData ? weatherData.marine.current[6][1] : "N/A"}
+                        units={"°"}
+                        text={"Swell Direction"}
+                        click={switchVisibility}
+                    />
+                    <ForecastButton
+                        safetynum={0}
+                        numval={weatherData ? weatherData.marine.current[7][1] : "N/A"}
+                        units={"m"}
+                        text={"Swell Height"}
                         click={switchVisibility}
                     />
                 </div>
-                </>
-               )}
+            </>
            
             <div id="LineChart" className={divDisp ? "hidden" : ""}>
                 {weatherData &&(
@@ -64,6 +134,7 @@ const App = () => {
                 )}
             </div>
         </div>
+        </>
     );
 };
 
